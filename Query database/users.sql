c5e197555ceb961df5a0502a5915150ac9047986
Najma Attaqiya Alya <71921144+Najma322@users.noreply.github.com>
2023-01-12 10:33:18 +0700

    Create users.sql

---
+++ Query database/users.sql
@@ -0,0 +1,55 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 100424 (10.4.24-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : bps_web_tracking
+
+ Target Server Type    : MySQL
+ Target Server Version : 100424 (10.4.24-MariaDB)
+ File Encoding         : 65001
+
+ Date: 12/01/2023 10:30:20
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_role_fk` int UNSIGNED NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `users_username_unique`(`username` ASC) USING BTREE,
+  UNIQUE INDEX `users_email_unique`(`email` ASC) USING BTREE,
+  INDEX `users_id_role_fk_foreign`(`id_role_fk` ASC) USING BTREE,
+  CONSTRAINT `users_id_role_fk_foreign` FOREIGN KEY (`id_role_fk`) REFERENCES `roles` (`id_role`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'Addel_01', 'addelsalindeho@gmail.com', '$2y$10$14f.B3wZvTmnt6YKRC3acuB9QkSOIHtbTC3bSbcIkyohSob.Y6nZy', 'Addel Salindeho', 2, NULL, '2023-01-10 08:33:07', '2023-01-10 08:33:07');
+INSERT INTO `users` VALUES (2, 'Yunus_petlap1', 'yunuspetlap1@gmail.com', '$2y$10$i8TSUp/jO7Pfj5Fgvkf3tObH61t2VTVVZNyoHadau5MbcVPn1VPa6', 'Yunus', 1, 'XvsWuVXn8OVtvQs6rQTAFc1ZoZYDj1yThVbOdpIx305Q7CsBCrs49rvGs5os', '2023-01-10 08:34:08', '2023-01-10 08:34:08');
+INSERT INTO `users` VALUES (3, 'Sukip_02', 'sukip@gmail.com', '$2y$10$OnYYlC6z4WkCVug5bvN02epnrSojMoxKySJICGdcVutsg9dMXaoJm', 'Sukip', 2, NULL, '2023-01-10 08:35:48', '2023-01-10 08:35:48');
+INSERT INTO `users` VALUES (4, 'eko_03', 'eko@gmail.com', '$2y$10$K/fw.9xqFrA889.XDDd66eLPdty02Xq9fT26FqaoPKsYn4Sg6xvyi', 'Eko', 2, NULL, '2023-01-10 08:44:23', '2023-01-10 08:44:23');
+INSERT INTO `users` VALUES (5, 'Ainul_petlap2', 'ainulpetlap2@gmail.com', '$2y$10$7g6L4T/HOz0FhOKJgugaOORT6wjiyJaLyXKXuVnRzkMzLsGnrw2mm', 'Ainul', 1, NULL, '2023-01-10 08:47:06', '2023-01-10 08:47:06');
+INSERT INTO `users` VALUES (6, 'Hadi_petlap3', 'hadipurpetlap3@gmail.com', '$2y$10$VDEaJ05q8z1ntZWjhuwb/uPpXGwAJ8i4apiXGQksZKGwx6Vui6kxS', 'Hadi Pur', 1, NULL, '2023-01-11 01:24:40', '2023-01-11 01:24:40');
+INSERT INTO `users` VALUES (7, 'Aab_petlap4', 'aabpetlap4@gmail.com', '$2y$10$qWMnjFc6kBROi7eM9PWkXOjwSNTxhY9ADprdZGfJmgvMjxyHSR9XG', 'Aab', 1, NULL, '2023-01-11 01:25:25', '2023-01-11 01:25:25');
+INSERT INTO `users` VALUES (8, 'Bram_petlap5', 'brampetlap5@gmail.com', '$2y$10$TFYPritsZ3TQaivLTwqOuOguQ4unlnGY./uODYr4m7DEGLvkS9xKS', 'Bram', 1, NULL, '2023-01-11 01:26:01', '2023-01-11 01:26:01');
+INSERT INTO `users` VALUES (9, 'Nur_petlap6', 'nurpetlap6@gmail.com', '$2y$10$E/YQidVqnJrFKL85R1h80OjEbKa.ZEpm3EKHo/DbpUCB71LITepa.', 'Nur', 1, NULL, '2023-01-11 01:26:37', '2023-01-11 01:26:37');
+INSERT INTO `users` VALUES (10, 'Henry_petlap7', 'henrypetlap7@gmail.com', '$2y$10$igCwR4PndQ5ahDzYrbpnR.Zitpjem4eNF.0tyIFz57dGJ/hOUbWiK', 'Henry', 1, NULL, '2023-01-11 01:27:17', '2023-01-11 01:27:17');
+
+SET FOREIGN_KEY_CHECKS = 1;
