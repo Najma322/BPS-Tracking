5a0aacee24bc9e2c7f9de6c73fdd54f9f196f693
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2023-01-16 15:30:24 +0700

    Add photos migration

---
+++ web_tracking/database/migrations/2023_01_09_034557_create_photos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plotting', function (Blueprint $table) {
+            $table->increments('id_plot');
+			$table->unsignedInteger('id_petlap_fk');
+            $table->unsignedInteger('id_supervisor_fk');
+			$table->integer('id_provinsi_fk');
+			$table->integer('id_kabupaten_fk');
+			$table->string('kode_nks_fk');
+			$table->smallInteger('ruta');
+			$table->smallInteger('state')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plotting');
+    }
+};
